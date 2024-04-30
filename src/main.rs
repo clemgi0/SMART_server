5fa56026488ca4b20660aa9d60c47e43ba541741
arosard <alexandre.rosard@gmail.com>
2024-04-30 09:04:05 +0200

    Simple Json return

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 #[macro_use] extern crate rocket;
+use rocket::serde::{Serialize, json::Json};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Task {
+    id: u32,
+    name: String,
+    position: String,
+}
+
+#[get("/todo")]
+fn todo() -> Json<Task> {
+    Json(Task { id: 25, name: String::from("Jean"), position: String::from("20 avenue Albert Einstein")})
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -7,5 +21,5 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index]).mount("/", routes![todo])
 }
