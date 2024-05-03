8529868d0f02ba83f13d664b6ea350554de2519b
galet.arthur <galet.arthur@gmail.com>
2024-05-03 08:55:10 +0200

    new endpoint add protection

--- src/request_data.rs
+++ src/request_data.rs
@@ -16,6 +16,13 @@ pub struct PositionData {
     pub longitude: f32,
 }
 
+#[derive(Deserialize)]
+pub struct NewProtectionData {
+    pub id_protector: i32,
+    pub id_protected: i32,
+    pub name_protected: String,
+}
+
 #[rocket::async_trait]
 impl<'r> FromData<'r> for ProtectionData {
     type Error = String;
@@ -45,6 +52,23 @@ impl<'r> FromData<'r> for PositionData {
 
         match serde_json::from_slice::<PositionData>(&bytes) {
             Ok(position_data) => Outcome::Success(position_data),
+            Err(_) => Outcome::Error((Status::UnprocessableEntity, "Invalid JSON format".to_string())),
+        }
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromData<'r> for NewProtectionData {
+    type Error = String;
+
+    async fn from_data(_req: &'r Request<'_>, data: Data<'r>) -> Outcome<'r, Self> {
+        let bytes = match data.open(ByteUnit::from(4096)).into_bytes().await {
+            Ok(bytes) => bytes,
+            Err(_) => return Outcome::Error((Status::InternalServerError, "Failed to read request body".to_string())),
+        };
+
+        match serde_json::from_slice::<NewProtectionData>(&bytes) {
+            Ok(new_protection_data) => Outcome::Success(new_protection_data),
             Err(_) => Outcome::Error((Status::UnprocessableEntity, "Invalid JSON format".to_string())),
         }
     }
